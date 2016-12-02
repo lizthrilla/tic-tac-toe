28f032e67bb0a2ff088bf0ac84cb82916765fcd7
Liz Tiller <elizabeth.tiller@gmail.com>
2016-12-02 16:17:52 -0500

    alternating between players and wins named

--- planofaction.md
+++ planofaction.md
@@ -1,28 +1,24 @@
 #Plan of action for Tic Tac textContent
 1. html create:
-*   h1 'let's play'
+<!-- *   h1 'let's play'
 *   3x3 table
-*   h3 player score
+*   h3 player turn -->
 *   button: "Turn Finished/play again"
-*   footer copyright
+<!-- *   footer copyright -->
 2.  CSS
 *   center everything
 *   table size
 *   modal alert (do after getting cells responsive)
 *   @keyframes and .winner, .loser (AFTER JAVASCRIPT)
 3.  Javascript
-*   make all game cells respond to a click
+<!-- *   make all game cells respond to a click
 *   alternate player turns
-- start player x
-- playerX marks an X in a cell
-- switch to playerO
-- playerO marks an O in a cell
-- playerX marks an X in a cell
-
-
-*   .textContent on h3 to signal if it's X or O
-*   if playerTurn === xPlayer then x marks the table
-*   if playerTurn === oPlayer then O marks the table
+-   start player x
+-   playerX marks an X in a cell
+-   switch to playerO
+-   playerO marks an O in a cell
+-   playerX marks an X in a cell -->
+<!-- *   .textContent on h3 to signal if it's X or O -->
 *   read board every time to see if winner
 *   define winner
 *   if winner.className('.winner')
--- src/index.js
+++ src/index.js
@@ -2,18 +2,40 @@ if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
 const cells = document.querySelectorAll('td')
-const playerTurn = document.querySelector('h3')
-
+const instructions = document.querySelector('h3')
 // const button = document.querySelector('button')
 
+let player = 'X'
+const win = [
+  ['t0', 't1', 't2'], ['t3', 't4', 't5'], ['t6', 't7', 't8'],
+  ['t0', 't3', 't6'], ['t1', 't4', 't7'], ['t2', 't5', 't8'],
+  ['t0', 't4', 't8'], ['t2', 't4', 't6']
+]
+console.log(win)
+
 const main = () => {
+  // let turn = true
+  // if (turn) {
   for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', () => {
-      cells[i].textContent = 'X'
-    })
+      // if (cells[i].textContent !== '') { // this will only work when I take the numbers out
+      cells[i].textContent = player
+      if (player === 'X') {
+        player = 'O'
+      } else {
+        player = 'X'
+      }
+      instructions.textContent = `Player ${player} Goes`
+    }
+    // }
+  )
   }
-}
+  instructions.textContent = 'Player X Goes'
 
+  // const gameOver () => {
+  //   if (document.querySelector('.top') === x)
+  // }
+}
 document.addEventListener('DOMContentLoaded', main)
 
 if (module.hot) {
