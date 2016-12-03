ed3290139bd4212e7fd939f8740da91f2ccb54b3
Liz Tiller <elizabeth.tiller@gmail.com>
2016-12-02 23:34:55 -0500

    modal styling done, button doesn't work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
-const cells = document.querySelectorAll('td')
-const instructions = document.querySelector('h3')
+const qs = (s) => document.querySelector(s)
 const qsa = (s) => document.querySelectorAll(s)
+const cells = qsa('td')
+const instructions = qs('h3')
+const button = qs('button')
+
 // const button = document.querySelector('button')
 
 const main = () => {
@@ -29,22 +32,38 @@ const main = () => {
     }
     )
   }
-  // instructions.textContent = 'Player X Goes'
-}
-const checkWin = () => {
-  if (qsa('td.p0[data-player=x]').length === 3 || qsa('td.p1[data-player=x]').length === 3 || qsa('td.p2[data-player=x]').length === 3 || qsa('td.p3[data-player=x]').length === 3 || qsa('td.p4[data-player=x]').length === 3 || qsa('td.p5[data-player=x]').length === 3 || qsa('td.p6[data-player=x]').length === 3 || qsa('td.p7[data-player=x]').length === 3) {
-    console.log('Player X wins')
-  } else if (qsa('td.p0[data-player=o]').length === 3 || qsa('td.p1[data-player=o]').length === 3 || qsa('td.p2[data-player=o]').length === 3 || qsa('td.p3[data-player=o]').length === 3 || qsa('td.p4[data-player=o]').length === 3 || qsa('td.p5[data-player=o]').length === 3 || qsa('td.p6[data-player=o]').length === 3 || qsa('td.p7[data-player=o]').length === 3) {
-    console.log('Player O wins')
+  const gameOver = (xWon) => {
+    if (xWon) {
+      document.querySelector('.dialog h3').textContent = 'Player X Won!'
+    } else {
+      document.querySelector('.dialog h3').textContent = 'Player O won!'
+    }
+    document.querySelector('body').className = 'modal'
   }
+
+  const checkWin = () => {
+    if (qsa('td.p0[data-player=x]').length === 3 || qsa('td.p1[data-player=x]').length === 3 || qsa('td.p2[data-player=x]').length === 3 || qsa('td.p3[data-player=x]').length === 3 || qsa('td.p4[data-player=x]').length === 3 || qsa('td.p5[data-player=x]').length === 3 || qsa('td.p6[data-player=x]').length === 3 || qsa('td.p7[data-player=x]').length === 3) {
+      console.log('Player X wins')
+      gameOver(true)
+      // instructions.textContent = 'Player X Won'
+    } else if (qsa('td.p0[data-player=o]').length === 3 || qsa('td.p1[data-player=o]').length === 3 || qsa('td.p2[data-player=o]').length === 3 || qsa('td.p3[data-player=o]').length === 3 || qsa('td.p4[data-player=o]').length === 3 || qsa('td.p5[data-player=o]').length === 3 || qsa('td.p6[data-player=o]').length === 3 || qsa('td.p7[data-player=o]').length === 3) {
+      console.log('Player O wins')
+      gameOver(false)
+      // instructions.textContent = 'Player O Won'
+    }
+  }
+  // const resetGame = () => {
+  //   qs('body').className = ' '
+  //   // for (let i = 0; i < cells.length; i++) {
+  //     cells[i].textContent = ' '
+  //   }
+  // }
 }
-
-// const gameOver = () => {
-//   if (check
-// }
-
-  // const gameOver () => {
-  //   if (document.querySelector('.top') === x)
+  // qsa('button').addEventListener('click', () => {
+  //   qs('body').className = ' '
+  //   for (let i = 0; i < cells.length; i++) {
+  //     cells[i].textContent = ' '
+  //     }
   // }
 
 document.addEventListener('DOMContentLoaded', main)
