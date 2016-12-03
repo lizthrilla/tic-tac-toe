28ea34a156992307eebb0c14751719646e5a0c98
Liz Tiller <elizabeth.tiller@gmail.com>
2016-12-02 19:05:01 -0500

    logic done

--- src/index.js
+++ src/index.js
@@ -3,33 +3,74 @@ import './styles/screen.scss'
 
 const cells = document.querySelectorAll('td')
 const instructions = document.querySelector('h3')
+const qsa = (s) => document.querySelectorAll(s)
 // const button = document.querySelector('button')
 
-let player = 'X'
 const main = () => {
-  // let turn = true
-  // if (turn) {
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].textContent = ' '
+  }
+  let player = 'x'
   for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', () => {
-      // if (cells[i].textContent !== '') {
       cells[i].textContent = player
-      if (player === 'X') {
-        player = 'O'
-        cells[i].dataset.player = 'X'
+      if (player === 'x') {
+        player = 'o'
+        cells[i].dataset.player = 'x'
+        console.log(`${cells[i].dataset.player}`)
+        checkWin()
       } else {
-        player = 'X'
-        cells[i].dataset.player = 'O'
+        player = 'x'
+        cells[i].dataset.player = 'o'
+        console.log(`${cells[i].dataset.player}`)
+        checkWin()
       }
       instructions.textContent = `Player ${player} Goes`
     }
     )
   }
-  instructions.textContent = 'Player X Goes'
+  // instructions.textContent = 'Player X Goes'
+}
+const checkWin = () => {
+  if (qsa('td.p0[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p1[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p2[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p3[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p4[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p5[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p6[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p7[data-player=x]').length === 3) {
+    console.log('Player X wins')
+  } else if (qsa('td.p0[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  } else if (qsa('td.p1[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  } else if (qsa('td.p2[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  } else if (qsa('td.p3[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  } else if (qsa('td.p4[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  } else if (qsa('td.p5[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  } else if (qsa('td.p6[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  } else if (qsa('td.p7[data-player=o]').length === 3) {
+    console.log('Player O wins')
+  }
+}
 
   // const gameOver () => {
   //   if (document.querySelector('.top') === x)
   // }
-}
+
 document.addEventListener('DOMContentLoaded', main)
 
 if (module.hot) {
